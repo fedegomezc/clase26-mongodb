b275f3ab8356396565cb27f5ddef82241625eda7
Federico Gomez Conde <fedeic8@gmail.com>
2023-09-07 18:26:41 -0300

    se crea un solo esquema y se modifica crearRespuesta()

--- src/controllers/encuestas.js
+++ src/controllers/encuestas.js
@@ -1,9 +1,8 @@
-import { create, findById, crearRespuesta, votar } from '../models/encuestas.js'
+import { create, crearRespuesta } from '../models/encuestas.js'
 
 export const createQuestion = async (req, res) => {
     try {
         const question = await create(req.body);
-        console.log(question);
         res.status(201).json(question);
     } catch (error) {
         res.status(500).json({ error: 'Error al crear la pregunta' });
@@ -12,11 +11,7 @@ export const createQuestion = async (req, res) => {
 
 export const createAnswer = async (req, res) => {
     try {
-        const pregunta = await findById(req.params.preguntaId);
-        if (!pregunta) {
-            return res.status(404).json({ error: 'Pregunta no encontrada' });
-        }
-        const respuesta = await crearRespuesta(pregunta, req.body);
+        const respuesta = await crearRespuesta(req.params.preguntaId, req.body);
         res.status(201).json(respuesta);
 
     } catch (error) {
@@ -25,16 +20,16 @@ export const createAnswer = async (req, res) => {
     }
 }
 
-export const voteAnswer = async (req, res) => {
-    try {
-        const respuesta = await findById(req.params.respuestaId);
-        if (!respuesta) {
-            return res.status(404).json({ error: 'Respuesta no encontrada' });
-        }
-        const respuestaActualizada = await votar(respuesta, req.body);
-        return res.status(200).json(respuestaActualizada);
-    } catch (error) {
-        res.status(500).json({ message: 'Error al intentar votar' });
-        console.error(error);
-    }
-}
+// export const voteAnswer = async (req, res) => {
+//     try {
+//         const respuesta = await findById(req.params.respuestaId);
+//         if (!respuesta) {
+//             return res.status(404).json({ error: 'Respuesta no encontrada' });
+//         }
+//         const respuestaActualizada = await votar(respuesta, req.body);
+//         return res.status(200).json(respuestaActualizada);
+//     } catch (error) {
+//         res.status(500).json({ message: 'Error al intentar votar' });
+//         console.error(error);
+//     }
+// }
--- src/models/encuestas.js
+++ src/models/encuestas.js
@@ -1,37 +1,13 @@
 import mongoose from '../config/mongo.js';
 const ObjectId = mongoose.Types.ObjectId;
 
-const respuestaSchema = new mongoose.Schema({
-    respuesta: String,
-    personas_que_respondieron: [{ name: String }]
-});
-
-const Respuesta = mongoose.model('Respuestas', respuestaSchema);
-
 const preguntaSchema = new mongoose.Schema({
     pregunta: String,
-    respuestas: [respuestaSchema],
-});
-
-//middleware para crear respuestas cuando se crea una pregunta
-preguntaSchema.pre('save', async function (next) {
-    if (this.respuestas.length === 0) {
-        return next();
-    }
-
-    const promises = this.respuestas.map(async (respuestaData) => {
-        const respuesta = new Respuesta(respuestaData);
-        await respuesta.save();
-        return respuesta;
-    });
-
-    try {
-        // Esperar a que se creen todas las respuestas antes de continuar
-        this.respuestas = await Promise.all(promises);
-        next();
-    } catch (error) {
-        next(error);
-    }
+    respuestas: [{
+        respuesta_id: ObjectId,
+        respuesta: String,
+        personas_que_respondieron: [{ name: String }]
+    }],
 });
 
 // modelo
@@ -46,32 +22,26 @@ export async function create(data) {
     }
 }
 
-export async function findById(id) {
+export async function crearRespuesta(idPregunta, data) {
     try {
-        const document = await Respuesta.findById(id) || Pregunta.findById(id);
-        return document;
-    } catch (error) {
-        throw (`imposible retornar ${error}`)
-    }
-}
-
-export async function crearRespuesta(pregunta, data) {
-    try {
-        const respuesta = new Respuesta(data);
-        pregunta.respuestas.push(respuesta);
+        const pregunta = await Pregunta.findById(idPregunta);
+        if (!pregunta) {
+            throw new Error('Pregunta no encontrada');
+        }
+        pregunta.respuestas.push(data);
         pregunta.save();
-        return respuesta;
+        return data;
     } catch (error) {
         throw (`imposible crear: ${error}`);
     }
 }
 
-export async function votar(respuesta, data) {
-    try {
-        respuesta.personas_que_respondieron.push(data);
-        respuesta.save();
-        return respuesta;
-    } catch (error) {
-        throw (`imposible actualizar: ${error}`);
-    }
-}
+// export async function votar(respuesta, data) {
+//     try {
+//         respuesta.personas_que_respondieron.push(data);
+//         respuesta.save();
+//         return respuesta;
+//     } catch (error) {
+//         throw (`imposible actualizar: ${error}`);
+//     }
+// }
